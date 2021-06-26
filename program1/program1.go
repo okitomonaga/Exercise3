e6882f5792c567a63f2872a550520ddc6f816e3b
okitomonaga <okinaga.tomohiro@learning-engineering.com>
2021-06-26 06:55:29 +0000

    implementation_program1

--- program1/program1.go
+++ program1/program1.go
@@ -1,24 +1,12 @@
 package main
 
-//import "fmt"
+import "fmt"
+
 //import "strconv"
 import "math"
 
 func getdigit(num int) []int {
-	// implementation here
-	/**
-	if i%15 == 0 {
-		return "FizzBuzz"
-	}
-	if i%3 == 0 {
-		return "Fizz"
-	}
-	if i%5 == 0 {
-		return "Buzz"
-	}
 
-	return strconv.Itoa(i)
-	*/
 	var digit_slice []int
 	for i := 0; i < 4; i++ {
 		pow := int(math.Pow(10, float64(3-i)))
@@ -29,11 +17,131 @@ func getdigit(num int) []int {
 	return digit_slice
 }
 
-/**
-func main() {
-	// implementation is not yet
-	for i := 1; i <= 100; i++ {
-		fmt.Println(fizzbuzz(i))
+func getstring1000(num int) string {
+
+	var result string
+	switch num {
+	case 1:
+		result = "M"
+	case 2:
+		result = "MM"
+	case 3:
+		result = "MMM"
+	case 4:
+		result = "MMMM"
+	default:
+		result = ""
 	}
+
+	return result
+
 }
-*/
+
+func getstring100(num int) string {
+
+	var result string
+	switch num {
+	case 1:
+		result = "C"
+	case 2:
+		result = "CC"
+	case 3:
+		result = "CCC"
+	case 4:
+		result = "CD"
+	case 5:
+		result = "D"
+	case 6:
+		result = "DC"
+	case 7:
+		result = "DCC"
+	case 8:
+		result = "DCCC"
+	case 9:
+		result = "CM"
+	default:
+		result = ""
+	}
+
+	return result
+
+}
+
+func getstring10(num int) string {
+
+	var result string
+	switch num {
+	case 1:
+		result = "X"
+	case 2:
+		result = "XX"
+	case 3:
+		result = "XXX"
+	case 4:
+		result = "XL"
+	case 5:
+		result = "L"
+	case 6:
+		result = "LX"
+	case 7:
+		result = "LXX"
+	case 8:
+		result = "LXXX"
+	case 9:
+		result = "XC"
+	default:
+		result = ""
+	}
+
+	return result
+
+}
+
+func getstring1(num int) string {
+
+	var result string
+	switch num {
+	case 1:
+		result = "I"
+	case 2:
+		result = "II"
+	case 3:
+		result = "III"
+	case 4:
+		result = "IV"
+	case 5:
+		result = "V"
+	case 6:
+		result = "VI"
+	case 7:
+		result = "VII"
+	case 8:
+		result = "VIII"
+	case 9:
+		result = "IX"
+	default:
+		result = ""
+	}
+
+	return result
+
+}
+
+func generate_string(num int) string {
+	var result string
+	digit_slice := make([]int, 4)
+	copy(digit_slice, getdigit(num))
+
+	result += getstring1000(digit_slice[0])
+	result += getstring100(digit_slice[1])
+	result += getstring10(digit_slice[2])
+	result += getstring1(digit_slice[3])
+	return result
+}
+
+func main() {
+	fmt.Println(generate_string(1990))
+	fmt.Println(generate_string(2008))
+	fmt.Println(generate_string(99))
+	fmt.Println(generate_string(47))
+}
--- program1/program1_test.go
+++ program1/program1_test.go
@@ -11,7 +11,6 @@ func TestGetdigit1(t *testing.T) {
 	result := getdigit(num)
 	fmt.Println(result)
 	if result[0] != 1 || result[1] != 4 || result[2] != 6 || result[3] != 7 {
-		//if result != []int{1, 4, 6, 7} {
 		t.Error("Testgetdigit1 is failed")
 	}
 }
@@ -21,7 +20,114 @@ func TestGetdigit2(t *testing.T) {
 	result := getdigit(num)
 	fmt.Println(result)
 	if result[0] != 2 || result[1] != 4 || result[2] != 5 || result[3] != 6 {
-		//if result != []int{1, 4, 6, 7} {
 		t.Error("Testgetdigit2 is failed")
+	}
+}
+
+func TestGetstring1000_1(t *testing.T) {
+	num := 1
+	result := getstring1000(num)
+	fmt.Println(result)
+	if result != "M" {
+		t.Error("Testgetsritng1000_1 is failed")
+	}
+}
+
+func TestGetstring1000_2(t *testing.T) {
+	num := 2
+	result := getstring1000(num)
+	fmt.Println(result)
+	if result != "MM" {
+		t.Error("Testgetsritng1000_2 is failed")
+	}
+}
+
+func TestGetstring100_1(t *testing.T) {
+	num := 5
+	result := getstring100(num)
+	fmt.Println(result)
+	if result != "D" {
+		t.Error("Testgetsritng100_1 is failed")
+	}
+}
+
+func TestGetstring100_2(t *testing.T) {
+	num := 4
+	result := getstring100(num)
+	fmt.Println(result)
+	if result != "CD" {
+		t.Error("Testgetsritng100_2 is failed")
+	}
+}
+
+func TestGetstring10_1(t *testing.T) {
+	num := 5
+	result := getstring10(num)
+	fmt.Println(result)
+	if result != "L" {
+		t.Error("Testgetsritng10_1 is failed")
+	}
+}
+
+func TestGetstring10_2(t *testing.T) {
+	num := 6
+	result := getstring10(num)
+	fmt.Println(result)
+	if result != "LX" {
+		t.Error("Testgetsritng10_2 is failed")
+	}
+}
+
+func TestGetstring10_3(t *testing.T) {
+	num := 0
+	result := getstring10(num)
+	fmt.Println(result)
+	if result != "" {
+		t.Error("Testgetsritng10_3 is failed")
+	}
+}
+
+func TestGetstring1_1(t *testing.T) {
+	num := 5
+	result := getstring1(num)
+	fmt.Println(result)
+	if result != "V" {
+		t.Error("Testgetsritng1_1 is failed")
+	}
+}
+
+func TestGetstring1_2(t *testing.T) {
+	num := 6
+	result := getstring1(num)
+	fmt.Println(result)
+	if result != "VI" {
+		t.Error("Testgetsritng1_2 is failed")
+	}
+}
+
+func TestGeneratestring01(t *testing.T) {
+	num := 1234
+	result := generate_string(num)
+	fmt.Println(result)
+	if result != "MCCXXXIV" {
+		t.Error("TestGeneratestring01 is failed")
+	}
+}
+
+func TestGeneratestring02(t *testing.T) {
+	num := 2345
+	result := generate_string(num)
+	fmt.Println(result)
+	if result != "MMCCCXLV" {
+		t.Error("TestGeneratestring02 is failed")
+	}
+}
+
+func TestGeneratestring03(t *testing.T) {
+	num := 1024
+	result := generate_string(num)
+	fmt.Println(result)
+	if result != "MXXIV" {
+		t.Error("TestGeneratestring03 is failed")
 	}
 }
